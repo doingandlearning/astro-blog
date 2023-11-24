4a16a0455fce9e1449bd2fa869d1c6ae44609142
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-24 12:52:51 +0000

    add initial code editor post

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "preact/hooks";
+import "./CodeEditor.css";
+
+export default function CodeEditor({ initialCode = "" }) {
+	const [code, setCode] = useState(initialCode);
+	const [display, setDisplay] = useState("");
+
+	const handleChange = (e) => {
+		setCode(e.currentTarget.value);
+
+	};
+
+	useEffect(() => {
+		try {
+			const result = eval(code);
+			setDisplay(result);
+		} catch (e) {
+			if (e instanceof Error) {
+				setDisplay(e.message);
+			}
+		}
+	}, [code])
+	return (
+		<div id="container">
+			<div id="editor">
+				<h2>Type some code in here</h2>
+				<textarea id="code" value={code} onKeyUp={handleChange}></textarea>
+			</div>
+			<div id="result-container">
+				<h2>See it evaluate here</h2>
+				<div id="result">
+					{display}
+				</div>
+			</div>
+		</div>
+	);
+}
