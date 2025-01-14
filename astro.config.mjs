a3348fd626bcbfbe2a8c0afb7b5a4f8404e8f48a
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-01-14 10:25:05 +0000

    Fixes a typing issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import expressiveCode from "astro-expressive-code";
 
 import customToc from "astro-custom-toc";
